cc2e18e9ea99e64ef942531865d134df8868aa5a
francosantzz <129998263+francosantzz@users.noreply.github.com>
2023-08-29 18:11:46 -0300

    Ejercicio 2

---
+++ ejer02/src/main/java/com/utn/ejer02/entidades/Persona.java
@@ -0,0 +1,29 @@
+package com.utn.ejer02.entidades;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Persona implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String apellido;
+    private int edad;
+
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "domicilio_id")
+    private Domicilio domicilio;
+}
